e18d6fce8bc3f3c565aff1eae6774ebe93e3bd95
Parleen Kaur <malhotraparleen96@gmail.com>
2022-07-31 01:22:26 -0700

    commit

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-//template_4hf3vbl
-//service_07rekzy
-//DpxkNVl4whdCwF9rf
-// emailjs
-//.sendForm(
-   // 'service_07rekzy',
-   // 'template_4hf3vbl',
-  //   event.target,
-  //  'user_DpxkNVl4whdCwF9rf'
-  // ).then(() => {
- //      console.log('this worked')
-//   })
-
-function contact(event){
+function contact(event) {
     event.preventDefault();
-   
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " .modal__overlay--visible"
-    setTimeout(() => {
-        console.log('it worked')
-    }, 500);
-   
-}
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+      .sendForm(
+        "service_80ih0if",
+        "template_d9refyl",
+        event.target,
+        "DpxkNVl4whdCwF9rf"
+      )
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+      })
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        );
+      });
+  }
+
+
+
+
